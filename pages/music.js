35a9e07ff00debd7217ee93e214201a2f683987c
axiply <swanishbaweja@gmail.com>
2024-07-29 11:27:21 -0400

    music upd

--- pages/music.js
+++ pages/music.js
@@ -4,9 +4,26 @@ const relaxAudio = new Audio('../assets/lofi-relax-travel-by-lofium-123560.mp3')
 const studyAudio = new Audio('../assets/lofi-study-112191.mp3');
 const summerAudio = new Audio('../assets/summer-rain-lofi-vibes-216043.mp3');
 
+const prevBtn = document.querySelector('.previous');
+const playBtn = document.querySelector('.play-pause');
+const nextBtn = document.querySelector('.next');
+const songName = document.querySelector('.song-name')
 
-const prevBtn = document.querySelector('.previous')
+const songs = [
+    { ele: dreamAudio, audioName: 'Dream Lofi Vibes'},
+    { ele: orchAudio, audioName: 'Orchestra Lofi Vibes'},
+    { ele: relaxAudio, audioName: 'Relax Lofi Vibes'},
+    { ele: studyAudio, audioName: 'Study Lofi Vibes'},
+    { ele: summerAudio, audioName: 'Summer Lofi Vibes'},
 
-summerAudio.addEventListener("canplaythrough", (event) => {
-    summerAudio.play();
-  })
+];
+
+let current = 0;
+let currentSong = songs[current].ele;
+songName.textContent = songs[current].audioName;
+
+playBtn.addEventListener('click', ()=> {
+    currentSong.play();
+})
+
+
