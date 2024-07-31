c73c6a609328adafeba6ad2d5eb9e7971f218eac
axiply <swanishbaweja@gmail.com>
2024-07-30 20:22:02 -0400

    Fixed Quotes (Thanks scuff)

--- src/quotes.js
+++ src/quotes.js
@@ -1,30 +1,33 @@
 const quotes = [{
-    quote: '"End is not the end if fact E.N.D. Means Efforts Never Dies."',
+    quote: 'End is not the end if fact E.N.D. Means Efforts Never Dies.',
     writer: 'Dr. A.P.J. Abdul Kalam'
 }, {
-    quote: '"A person who never made a mistake never tried anything new."',
+    quote: 'A person who never made a mistake never tried anything new.',
     writer: 'Albet Einstein'
 }, {
-    quote: '"There are plenty of difficult obstacles in your path. Do not allow yourself to become one of them."',
+    quote: 'There are plenty of difficult obstacles in your path. Do not allow yourself to become one of them.',
     writer: 'Ralph Marston'
 }, {
-    quote: '"Only I can change my life. No one can do it for me."',
+    quote: 'Only I can change my life. No one can do it for me.',
     writer: 'Carol Burnett'
 }, {
-    quote: '"I think I can. I know I can."',
+    quote: 'I think I can. I know I can.',
     writer: 'Jennifer Wittwer'
 }, {
-    quote: '"You must be the change you wish to see in the world."',
+    quote: 'You must be the change you wish to see in the world.',
     writer: 'Mahatma Gandhi'
 }, {
-    quote: '"Do not wait for the opportunity. Create it."',
+    quote: 'Do not wait for the opportunity. Create it.',
     writer: 'George Bernard Shaw'
 }, {
-    quote: '"Self-belief and hard work will always earn you success."',
+    quote: 'Self-belief and hard work will always earn you success.',
     writer: 'Virat Kohli'
 }, {
-    quote: '"When you reach the end of your rope, tie a knot in it and hang on."',
+    quote: 'When you reach the end of your rope, tie a knot in it and hang on.',
     writer: 'Franklin D. Roosevelt'
+},{
+    quote: 'If you get tired, learn to rest not to quit.',
+    writer: 'Banksy'
 }
 ]
 
