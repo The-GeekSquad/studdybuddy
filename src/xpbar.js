4effc022def55102db6793a5791fa48b11c8bc9b
Sukhdeep Somal <117871204+TwistedMiraculous@users.noreply.github.com>
2024-07-31 19:11:47 -0700

    modified:   pages/xpbar.html 	modified:   src/xpbar.js 	modified:   styles/xpbar.css

--- src/xpbar.js
+++ src/xpbar.js
@@ -1,10 +1,17 @@
 let currentXP = 0;
 const maxXP = 100;
+let currentLevel = 1;
 
 function addXP(amount) {
   currentXP += amount;
-  if (currentXP > maxXP) currentXP = maxXP;
-  
+  if (currentXP >= maxXP) {
+    levelUp();
+  } else {
+    updateXPBar();
+  }
+}
+
+function updateXPBar() {
   const xpFill = document.getElementById('xp-fill');
   const xpText = document.getElementById('xp-text');
   
@@ -12,3 +19,23 @@ function addXP(amount) {
   xpFill.style.width = percentage + '%';
   xpText.innerText = `XP: ${currentXP}/${maxXP}`;
 }
+
+function levelUp() {
+  currentLevel++;
+  currentXP = 0;
+  document.getElementById('level-text').innerText = `Level: ${currentLevel}`;
+  
+  showLevelUpImage();
+}
+
+function showLevelUpImage() {
+  const levelUpImage = document.getElementById('level-up-image');
+  levelUpImage.classList.remove('hidden');
+  
+  setTimeout(() => {
+    levelUpImage.classList.add('hidden');
+    updateXPBar();
+  }, 7000); // Show the image for 7 seconds
+}
+
+updateXPBar();
